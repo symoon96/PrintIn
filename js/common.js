e1b7a30f8462502961cce3f9ded45a9415964ed4
symoon96 <mono4682@gmail.com>
2024-06-24 23:57:20 +0900

    수정

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    accList()
+    accList();
+    tab()
 })
 
 function toggleAllGnb(el){
@@ -64,22 +65,31 @@ function bodyAuto() {
 }
 
 // 팝업열기
-function popOpen(id){
-    $("#" + id).addClass('on');
+function popOpen(el){
+    $("." + el).addClass('on');
 }
 
 // 팝업닫기
-function popClose(id) {
-    $("#" + id).removeClass('on');
+function popClose(el) {
+    $("." + el).removeClass('on');
     dimRemove();
 }
 
 // dim 옵션 팝업 열기
-function popOpenAndDim(id, isDim){
-    popOpen(id);
+function popOpenAndDim(el, isDim){
+    popOpen(el);
     
     if(isDim == true){
         dimMaker();
+    }
+}
+
+// dim 옵션 팝업 닫기
+function popCloseAndDim(el, isDim){
+    popClose(el);
+    
+    if(isDim == true){
+        dimRemove();
     }
 }
 
@@ -90,5 +100,43 @@ function accList(){
         $(this).closest('.acc-list').children('.acc-cont').slideToggle(300);
         $(this).closest('.acc-list').siblings().removeClass('on');
         $(this).closest('.acc-list').siblings().children('.acc-cont').slideUp(300);
+    });
+}
+
+// 탭
+function tab(){
+    $('.tab-wrap').each(function(){
+        let thisUse = $(this).data('use'),
+            thisNo = $(this).find('.tab-btn.on').index();
+
+        if(thisUse !== false) {
+            $(this).children('.tab-cont-wrap').children('.tab-cont').hide()
+            $(this).children('.tab-cont-wrap').children('.tab-cont').eq(thisNo).show();
+            
+            $(this).find('.tab-btn').click(function(){
+                thisNo = $(this).index();
+
+                $(this).siblings('.tab-btn').removeClass('on');
+                $(this).addClass('on');
+
+                $(this).closest('.tab-wrap').children('.tab-cont-wrap').children('.tab-cont').hide()
+                $(this).closest('.tab-wrap').children('.tab-cont-wrap').children('.tab-cont').eq(thisNo).show();
+            })
+
+            if($('[data-tab]').length > 0){
+                $('[data-tab]').each(function(){
+    
+                    $(this).click(function(){
+                        thisTabNo = $(this).data('tab') - 1;
+
+                        $(this).closest('.tab-wrap').find('.tab-cont').hide()
+                        $(this).closest('.tab-wrap').find('.tab-cont').eq(thisTabNo).show();
+        
+                        $(this).closest('.tab-wrap').find('.tab-btn').removeClass('on');
+                        $(this).closest('.tab-wrap').find('.tab-btn').eq(thisTabNo).addClass('on');
+                    })
+                })
+            }
+        }
     });
 }
