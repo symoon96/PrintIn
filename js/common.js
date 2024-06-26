79481422b5e9774b606c0d6a03a7bae27bf403a1
symoon96 <mono4682@gmail.com>
2024-06-26 22:15:32 +0900

    완료

--- js/common.js
+++ js/common.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     accList();
-    tab()
+    tab();
+    dateFormatChange()
 })
 
 function toggleAllGnb(el){
@@ -138,5 +139,18 @@ function tab(){
                 })
             }
         }
+    });
+}
+
+function dateFormatChange(){
+    $('.date input[type="date"]').change(function(){
+        let date = $(this).val();
+        let dateYear = date.substr(0, 4);
+        let dateMonth = date.substr(5, 2);
+        let dateDay = date.substr(8, 2);
+
+        $(this).closest('.date').find('input[type="text"]').val(dateYear + '.' + dateMonth + '.' + dateDay)
+
+        console.log(dateYear, dateMonth, dateDay)
     });
 }
